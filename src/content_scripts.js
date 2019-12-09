09ae16a87587c750d431e41e3ec069ad23a12990
omohayui <omohayui@gmail.com>
2019-12-10 02:19:52 +0900

    Add src

---
+++ src/content_scripts.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".incident > tbody > tr")
+.forEach(row => {
+    const date = row.querySelector("td.date")
+    const time = row.querySelector("td.time") 
+    if (date && time) {
+        const datetime = new Date(date.innerHTML + ' ' + time.innerHTML + ' PST');
+        date.innerHTML = datetime.toDateString()
+        time.innerHTML = datetime.toTimeString()
+    }
+});
