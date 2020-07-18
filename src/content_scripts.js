470c0aede0f7b99cd780ac7cf752c191cff792db
omohayui <omohayui@gmail.com>
2020-07-19 00:05:55 +0900

    Fix temporary summer time zone

--- src/content_scripts.js
+++ src/content_scripts.js
@@ -1,9 +1,9 @@
 document.querySelectorAll(".incident > tbody > tr")
 .forEach(row => {
     const date = row.querySelector("td.date")
-    const time = row.querySelector("td.time") 
+    const time = row.querySelector("td.time")
     if (date && time) {
-        const datetime = new Date(date.innerHTML + ' ' + time.innerHTML + ' PST');
+        const datetime = new Date(date.innerHTML + ' ' + time.innerHTML + ' PDT');
         date.innerHTML = datetime.toDateString()
         time.innerHTML = datetime.toTimeString()
     }
