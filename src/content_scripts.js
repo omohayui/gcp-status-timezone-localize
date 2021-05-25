accd84b38cda9ae1258768889b32a2391631d5c3
omohayui <omohayui@gmail.com>
2021-05-25 17:00:16 +0900

    fix parsed elements and release v1.0.3

--- src/content_scripts.js
+++ src/content_scripts.js
@@ -1,9 +1,9 @@
-document.querySelectorAll(".incident > tbody > tr")
+document.querySelectorAll(".status-updates > tbody > tr")
 .forEach(row => {
     const date = row.querySelector("td.date")
     const time = row.querySelector("td.time")
     if (date && time) {
-        const datetime = new Date(date.innerHTML + ' ' + time.innerHTML + ' PDT');
+        const datetime = new Date(date.innerHTML + ' ' + time.innerHTML);
         date.innerHTML = datetime.toDateString()
         time.innerHTML = datetime.toTimeString()
     }
